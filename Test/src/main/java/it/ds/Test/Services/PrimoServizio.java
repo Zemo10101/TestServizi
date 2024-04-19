f7ddf963e7bdcec45982d9179b37181042748e82
Zemo <159175205+Zemo10101@users.noreply.github.com>
2024-04-19 13:39:14 +0200

    aggiunto secondo servizio

--- Test/src/main/java/it/ds/Test/Services/PrimoServizio.java
+++ Test/src/main/java/it/ds/Test/Services/PrimoServizio.java
@@ -3,11 +3,12 @@ package it.ds.Test.Services;
 import it.ds.Test.Interfaccia.optionInterfaccia;
 
 public class PrimoServizio implements optionInterfaccia {
-    private String name;
+
+    private final String NAME="primoServizio";
 
     @Override
     public String getName() {
-        return name;
+        return NAME;
     }
 
     @Override
--- Test/src/main/java/it/ds/Test/Services/SecondoServizio.java
+++ Test/src/main/java/it/ds/Test/Services/SecondoServizio.java
@@ -1,4 +1,18 @@
 package it.ds.Test.Services;
 
-public class SecondoServizio {
+import it.ds.Test.Interfaccia.optionInterfaccia;
+
+public class SecondoServizio implements optionInterfaccia {
+
+    private final String NAME="secondoServizio";
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public int elabora(int a, int b, int c) {
+        return a-b-c;
+    }
 }

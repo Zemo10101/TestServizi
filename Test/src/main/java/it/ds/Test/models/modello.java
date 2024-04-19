714a84afdc4deefe1d0e05419df9142ad7099c4f
matteoprogr <matteo93parisi@gmail.com>
2024-04-19 11:12:34 +0200

    aggiunta classe modello

---
+++ Test/src/main/java/it/ds/Test/models/modello.java
@@ -0,0 +1,16 @@
+package it.ds.Test.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class modello {
+
+    int a;
+    int b;
+    int c;
+    String chiave;
+}
